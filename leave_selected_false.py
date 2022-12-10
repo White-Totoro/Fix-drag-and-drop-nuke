4447f98aed52b4de28e97d93aa1678bd7c8b5861
White-Totoro <109896841+White-Totoro@users.noreply.github.com>
2022-12-10 23:25:20 +0300

    Add files via upload

---
+++ leave_selected_false.py
@@ -0,0 +1,40 @@
+# --------------------------------------------------------------
+#  leave_selected_false.py
+#  Version: 1.0.1
+#  Author: Alexander Marchenko
+#
+#  Last Modified by: Alexander Marchenko
+#  Last Updated: December 12th, 2022
+# -------------------------------------------------------------
+
+import nuke
+
+from PySide2.QtCore import *
+from PySide2.QtWidgets import *
+from PySide2.QtGui import *
+
+nuke_app = QApplication.instance()
+
+class eventFilterWindowClass(QObject):
+    def eventFilter(self, obj, ev):
+        """Check evene leave mouse outside the windoW """
+        
+        if ev.type() == QEvent.Leave:
+            for n in nuke.allNodes():
+                n.knob('selected').setValue(False)
+        return False
+
+def get_main_window():
+    """This function get UI
+
+    Returns:
+        Object: get access to the main window 
+    """
+    for widget in nuke_app.topLevelWidgets():
+        if widget.metaObject().className() == 'Foundry::UI::DockMainWindow':
+            return widget
+
+#Install eventFilterWindowClass
+main_window = get_main_window()
+ev = eventFilterWindowClass()
+main_window.installEventFilter(ev)
